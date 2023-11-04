d25408baca1417d82bb07e3596a8ac33a18f2d80
fatima <fadam@ewu.edu>
2023-11-04 12:09:57 -0700

    hello world

---
+++ src/User.java
@@ -0,0 +1,27 @@
+//import math
+
+public class User {
+    private String name; // first and last name
+    private int age;
+    private char bloodType;
+    private int bloodPressure;
+    private char gender; // ie F = female
+    private double weight; // person's weight in lbs
+    private double fatPercent;
+//    (weight/height)^2 = BMI
+
+    // default constructor
+    public User(){}
+
+    public int getBMI(){
+        double BMI;
+        return 0 ;
+    }
+
+    // Record the systolic pressure over diastolic pressure, such as "120/80 mm Hg."
+    public boolean isHypertension(){
+        return false;
+    }
+
+
+}
